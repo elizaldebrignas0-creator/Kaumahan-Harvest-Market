3fce7d816d1de5531a5784167a68b6663db877fb
elizaldebrignas0-creator <elizaldebrignas0@gmail.com>
2025-11-22 23:59:49 +0800

    Simplify deployment config

--- kaumahan/production_settings.py
+++ kaumahan/production_settings.py
@@ -1,21 +1,12 @@
+from .settings import *
 import os
 import dj_database_url
-from .settings import *
 
-# Override for production
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key')
 
-# Database
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
-# Security
-SECRET_KEY = os.environ.get('SECRET_KEY')
-SECURE_SSL_REDIRECT = False
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export DJANGO_SETTINGS_MODULE=kaumahan.production_settings
 python manage.py migrate
-gunicorn kaumahan.wsgi:application --bind 0.0.0.0:8000
+gunicorn kaumahan.wsgi:application --bind 0.0.0.0:$PORT
