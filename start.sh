071fccd5692392af2932b2fece3e355df830ca13
elizaldebrignas0-creator <elizaldebrignas0@gmail.com>
2025-11-23 02:11:20 +0800

    Switch to make existing user admin

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running migrations..."
 python manage.py migrate
-echo "Creating new admin..."
-python create_new_admin.py
+echo "Making user admin..."
+python make_admin.py
 echo "Starting server..."
 gunicorn kaumahan.wsgi:application --bind 0.0.0.0:$PORT
